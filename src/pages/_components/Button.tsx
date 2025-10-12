624d4f80f1d1aba357209839a53d4bf5d6d73ad9
efuas <efuas33@gmail.com>
2025-10-12 22:10:11 +0000

    feat: enhance Button component with additional variants and sizes; update usage in App

--- src/pages/_components/Button.tsx
+++ src/pages/_components/Button.tsx
@@ -1,18 +1,66 @@
 import { forwardRef, type ButtonHTMLAttributes } from "react";
 import { type LucideIcon } from "lucide-react";
+import { cn } from "@sglara/cn";
 
 type ButtonProps = {
   label: string;
   icon?: LucideIcon;
   iconPosition?: "left" | "right";
+  variant?:
+    | "primary"
+    | "secondary"
+    | "destructive"
+    | "outline"
+    | "ghost"
+    | "link"
+    | "success"
+    | "warning"
+    | "danger";
+  size?: "sm" | "md" | "lg" | "default" | "icon" | "xs" | "xl";
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ label, icon: Icon, iconPosition = "left", className, ...props }, ref) => {
+  (
+    {
+      label,
+      size = "default",
+      variant = "primary",
+      icon: Icon,
+      iconPosition = "left",
+      className,
+      ...props
+    },
+    ref,
+  ) => {
     return (
       <button
         ref={ref}
-        className={`rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${className}`}
+        className={cn(
+          "inline-flex gap-2 hover:cursor-pointer items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+          className,
+          size === "default" && "h-10 px-4 py-2",
+          size === "sm" && "h-9 rounded-md px-3",
+          size === "lg" && "h-11 rounded-md px-8",
+          size === "icon" && "h-10 w-10",
+          size === "xs" && "h-8 rounded-md px-2 text-xs",
+          size === "xl" && "h-12 rounded-md px-10 text-base",
+          variant === "primary" && "text-white bg-primary hover:bg-primary/90",
+          variant === "secondary" &&
+            "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          variant === "destructive" &&
+            "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          variant === "outline" &&
+            "border border-input hover:bg-accent hover:text-accent-foreground",
+          variant === "ghost" && "hover:bg-accent hover:text-accent-foreground",
+          variant === "link" &&
+            "text-primary underline-offset-4 hover:underline",
+          variant === "success" &&
+            "bg-success text-success-foreground hover:bg-success/90",
+          variant === "warning" &&
+            "bg-warning text-warning-foreground hover:bg-warning/90",
+          variant === "danger" &&
+            "bg-error text-error-foreground hover:bg-error/90",
+        )}
         {...props}
       >
         {Icon && iconPosition === "left" && <Icon size={16} />}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import { FileText } from "lucide-react";
 import Button from "./_components/Button";
 
 function App() {
   return (
     <>
-      <Button label="Get Quote" />
+      <Button label="Get Quote" iconPosition="left" icon={FileText} />
     </>
   );
 }
